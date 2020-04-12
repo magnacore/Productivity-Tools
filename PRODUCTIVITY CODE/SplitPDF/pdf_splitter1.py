4ff97967e4b839074aeda00fe7ee15aad0039e1d
Manuj Chandra <manujchandra@gmail.com>
2020-04-12 16:45:56 +0530

    update scripts
    Add new scripts and make minor tweaks to existing scripts

--- PRODUCTIVITY CODE/SplitPDF/pdf_splitter1.py
+++ PRODUCTIVITY CODE/SplitPDF/pdf_splitter1.py
@@ -1,5 +1,5 @@
 # pdf_splitter.py
- 
+
 import os
 import glob
 from PyPDF2 import PdfFileReader, PdfFileWriter
@@ -22,9 +22,9 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     bar = fill * filledLength + '-' * (length - filledLength)
     print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
     # Print New Line on Complete
-    if iteration == total: 
+    if iteration == total:
         print()
- 
+
 def pdf_splitter(paths):
 
 	for path in paths:
@@ -33,18 +33,18 @@ def pdf_splitter(paths):
 		pdf = PdfFileReader(path)
 
 		pdf_writer = PdfFileWriter()
-		
-		for page in range(pdf.getNumPages()):		
+
+		for page in range(pdf.getNumPages()):
 			pdf_writer.addPage(pdf.getPage(page))
-						
-			output_filename = "{}_page_{}.pdf".format(fname, page + 1)
-			
+
+			output_filename = f"{fname}_page_{str(page + 1).zfill(4)}.pdf"
+
 			with open(output_filename, 'wb') as out:
 				pdf_writer.write(out)
-				
+
 			pdf_writer = PdfFileWriter()
-			
-			printProgressBar(page+1, pdf.getNumPages(), prefix = 'Progress:', suffix = 'Complete', length = 50)			
+
+			printProgressBar(page+1, pdf.getNumPages(), prefix = 'Progress:', suffix = 'Complete', length = 50)
 
 if __name__ == '__main__':
     paths = glob.glob('*.pdf')
