301ebc19142ffd7a8d286406b1e856021eb7e920
Manuj Chandra <manujchandrawork@gmail.com>
2021-11-17 12:34:13 +0530

    Update config.py

--- Linux/config/qtile/config.py
+++ Linux/config/qtile/config.py
@@ -34,7 +34,7 @@ import subprocess
 from libqtile import bar, layout, widget, hook, qtile
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
+# from libqtile.utils import guess_terminal
 
 
 mod = "mod4"
