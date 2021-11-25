40d4f1cefa52f7b6e1b26232e30004adf8144c19
Manuj Chandra <manujchandrawork@gmail.com>
2021-11-25 12:33:19 +0530

    Enable svg preview

--- Linux/config/ranger/scope.sh
+++ Linux/config/ranger/scope.sh
@@ -127,16 +127,16 @@ handle_image() {
 
     local mimetype="${1}"
     case "${mimetype}" in
-        ## SVG
-        # image/svg+xml|image/svg)
-        #     convert -- "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
-        #     exit 1;;
+        # SVG
+         image/svg+xml|image/svg)
+             convert -- "${FILE_PATH}" "${IMAGE_CACHE_PATH}" && exit 6
+             exit 1;;
 
-        ## DjVu
-        # image/vnd.djvu)
-        #     ddjvu -format=tiff -quality=90 -page=1 -size="${DEFAULT_SIZE}" \
-        #           - "${IMAGE_CACHE_PATH}" < "${FILE_PATH}" \
-        #           && exit 6 || exit 1;;
+        # DjVu
+         image/vnd.djvu)
+             ddjvu -format=tiff -quality=90 -page=1 -size="${DEFAULT_SIZE}" \
+                   - "${IMAGE_CACHE_PATH}" < "${FILE_PATH}" \
+                   && exit 6 || exit 1;;
 
         ## Image
         image/*)
