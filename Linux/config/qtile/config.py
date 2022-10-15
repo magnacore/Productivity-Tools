c1b7335691bb31cd149ef37f6fbf71568412e24a
Manuj Chandra <manujchandrawork@gmail.com>
2022-10-15 16:31:10 +0530

    Open Firefox in group 3

--- Linux/config/qtile/config.py
+++ Linux/config/qtile/config.py
@@ -90,7 +90,7 @@ keys = [
 # Run xprop | grep WM_CLASS | awk '{print $4}' in terminal to find wm_class
 groups = [Group("1", layout='treetab', matches=[Match(wm_class=["Ferdium", "Ghb", "Thunderbird", "Transmission-gtk"])]),
           Group("2", layout='bsp'),
-          Group("3", layout='bsp', matches=[Match(wm_class=["Firefox"])]),
+          Group("3", layout='bsp', matches=[Match(wm_class=[myBrowser])]),
           Group("4", layout='max'),
           Group("5", layout='bsp'),
           Group("6", layout='bsp'),
