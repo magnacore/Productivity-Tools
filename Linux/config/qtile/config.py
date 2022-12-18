29f7c9442a67fa019595305d494a8b5527bb9326
Manuj Chandra <manujchandrawork@gmail.com>
2022-12-18 18:10:02 +0530

    Insert urls from clipboard into a text file for downloading

--- Linux/config/qtile/config.py
+++ Linux/config/qtile/config.py
@@ -69,6 +69,7 @@ keys = [
 	Key([mod], "b", lazy.spawn(myBrowser), desc='My Browser' ),
     Key([mod, "shift"], "c", lazy.spawn(myTerm+" --disable-server -e 'flatpak run com.github.miguelmota.Cointop'"), desc='Cointop' ),
     Key([mod], "d", lazy.spawn(f"{myhome}/anaconda3/envs/util/bin/python {myhome}/Bin/clipboard-convert-text"), desc="Save clipboard to text"),
+    Key([mod], "y", lazy.spawn(f"{myhome}/anaconda3/envs/util/bin/python {myhome}/Bin/clipboard-insert-link"), desc="Insert URLs in a text file"),
     Key([mod, "shift"], "m", lazy.spawn(f"bash {myhome}/Software/CMapTools/bin/CmapTools"), desc="Launch Cmap"),
 
     ## Rofi
--- Linux/xonsh/py_utilities.py
+++ Linux/xonsh/py_utilities.py
@@ -1,3 +1,5 @@
+import re
+
 def get_valid_filename(value, allow_unicode=False):
 	"""
 	Taken from https://github.com/django/django/blob/master/django/utils/text.py
@@ -24,3 +26,10 @@ def get_valid_filename(value, allow_unicode=False):
 	# because Windoze is case insensitive. This is to prevent file overwrite.
 	value = re.sub(r"[^\w\s-]", "", value.lower())
 	return re.sub(r"[-\s]+", "-", value).strip("-_")
+
+def find_urls(string):
+    # findall() has been used
+    # with valid conditions for urls in string
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    url = re.findall(regex, string)
+    return [x[0] for x in url]
