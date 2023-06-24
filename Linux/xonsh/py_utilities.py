1eb8fc71c8add0399e2b3278caa2546ad483e53a
Manuj Chandra <manujchandrawork@gmail.com>
2023-06-24 19:13:09 +0530

    Update clipboard-convert-text and py_utilities.py

--- Linux/xonsh/py_utilities.py
+++ Linux/xonsh/py_utilities.py
@@ -33,3 +33,14 @@ def find_urls(string):
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex, string)
     return [x[0] for x in url]
+
+def get_uuid():
+    import uuid
+    return str(uuid.uuid4())
+
+def get_timestamp():
+	from datetime import datetime
+
+	date_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+	return date_time
