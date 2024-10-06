528c8754488e9e49327b74dc364b20b8e9ad2e13
Manuj Chandra <manujchandrawork@gmail.com>
2024-10-06 11:32:55 +0530

    Update config.py

--- Linux/config/qtile/config.py
+++ Linux/config/qtile/config.py
@@ -83,6 +83,7 @@ keys = [
 	Key([], "XF86AudioMute", lazy.spawn("amixer -D pipewire sset Master toggle")),
     Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -D pipewire sset Master 1%-")),
     Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -D pipewire sset Master 1%+")),
+    Key([mod], "a", lazy.spawn("amixer set Master 20%")),
 
     ## Scratchpads
     Key([mod2, mod3], "a", lazy.spawn(myTerm+f" --disable-server -e 'nvim {myhome}/Productivity_System/TODO.txt'"), desc="Launch TODO List"),
@@ -90,9 +91,9 @@ keys = [
 ]
 
 # Run xprop | grep WM_CLASS | awk '{print $4}' in terminal to find wm_class
-groups = [Group("1", layout='treetab', matches=[Match(wm_class=["Ferdium", "Ghb", "Thunderbird", "Transmission-gtk"])]),
+groups = [Group("1", layout='treetab', matches=[Match(wm_class=["Ferdium", "fr.handbrake.ghb", "thunderbird-esr", "Transmission-gtk"])]),
           Group("2", layout='bsp'),
-          Group("3", layout='bsp', matches=[Match(wm_class=['Firefox-esr'])]),
+          Group("3", layout='bsp', matches=[Match(wm_class=['firefox-esr'])]),
           Group("4", layout='max'),
           Group("5", layout='bsp'),
           Group("6", layout='bsp'),
