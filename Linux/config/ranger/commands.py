b5b941a23d3a2912ce7ebe161c66d0106313f86d
Manuj Chandra <manujchandrawork@gmail.com>
2026-05-27 20:25:37 +0530

    Update commands.py

--- Linux/config/ranger/commands.py
+++ Linux/config/ranger/commands.py
@@ -1266,4 +1266,27 @@ class file_copy_similar(Command):
 
 ###############################################################################
 
+class mount(Command):
+    """:mount.
 
+    Show menu to mount and unmount.
+    """
+
+    MMTUI_PATH = "mmtui"
+
+    def execute(self):
+        """Show menu to mount and unmount."""
+        import os
+        import tempfile
+        (f, p) = tempfile.mkstemp()
+        os.close(f)
+        self.fm.execute_console(
+            f'shell bash -c "{self.MMTUI_PATH} 1> {p}"'
+        )
+        with open(p, 'r') as f:
+            d = f.readline().strip()
+            if os.path.exists(d):
+                self.fm.cd(d)
+        os.remove(p)
+
+###############################################################################
